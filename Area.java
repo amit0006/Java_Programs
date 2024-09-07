5599e5bd930272d63dcbff059f121130a1c20e88
Amit Kumar Thakur <kumaramitthakur563@gmail.com>
2024-09-07 19:58:40 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -17,32 +17,3 @@ class Area
                 System.out.println("The area of the given triangle is "+area);
         }
 }
-/*import java.util.Scanner;
-
-class Area {
-    public static void main(String[] args) {
-        int a, b, c;
-        float s;
-        double area;
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter the sides of the triangle:");
-        a = sc.nextInt();
-        b = sc.nextInt();
-        c = sc.nextInt();
-
-        // Closing the scanner after use
-        sc.close();
-
-        // Calculating semi-perimeter
-        s = (a + b + c) / 2f;
-
-        // Using Heron's formula to calculate the area
-        area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-
-        // Printing the area
-        System.out.printf("The area of the given triangle is %.2f\n", area);
-    }
-}*/
-
